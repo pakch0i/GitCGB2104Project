30f84af555b0e907a9f2e0fb4d4037ebd65decd3
怦然 <li-wenchuan@gitee.com>
2021-07-21 09:10:23 +0800

    git 自定义登录页面

---
+++ 02-jt-spring-security/src/main/java/com/cy/jt/security/config/RedirectAuthenticationSuccessHandler.java
@@ -0,0 +1,20 @@
+package com.cy.jt.security.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RedirectAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private String redirectUrl;
+    public RedirectAuthenticationSuccessHandler(String redirectUrl){
+        this.redirectUrl = redirectUrl;
+    }
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+        httpServletResponse.sendRedirect(redirectUrl);
+    }
+}
--- 02-jt-spring-security/src/main/java/com/cy/jt/security/config/SecurityConfig.java
+++ 02-jt-spring-security/src/main/java/com/cy/jt/security/config/SecurityConfig.java
@@ -2,13 +2,39 @@ package com.cy.jt.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
  */
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        //1.关闭跨域攻击
+        http.csrf().disable();
+        //2.配置登录url
+        http.formLogin()
+                .loginPage("/login.html")//登录页面
+                .loginProcessingUrl("/login")//与form表单中的action值相同
+                //.usernameParameter("username")//与form表单中input元素的name属性相同
+               // .passwordParameter("password")
+                .defaultSuccessUrl("/index.html");//登录成功后的url地址
+                //.failureUrl("/login.html?error");//登录失败(默认)
+//               .successForwardUrl("/index.html");
+                //.successHandler(new RedirectAuthenticationSuccessHandler ("http://www.tedu.cn"));
+        //3.放行登录url(不需要认证就可以访问)
+        http.authorizeRequests()
+                .antMatchers("/login.html","/index.html")//这里写要放行的资源
+                .permitAll()//允许访问
+                .anyRequest().authenticated();//除了以上的资源必须认证才可以访问
+    }
+
     /**
      * @Bean 注解通常会在@Configuration注解描述的类中描述方法
      * 用于告诉Spring框架这个方法的返回值会交给spring管理,并给
